657b7bb4531898447b4d0675538458a2467e10ee
MoazamAli45 <21-se-48@students.uettaxila.edu.pk>
2024-01-17 16:23:18 +0500

    Update Timeline component styles

--- app/components/Timeline/Timeline.jsx
+++ app/components/Timeline/Timeline.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Timeline = () => {
   return (
     <>
-      <div className="hidden custom-md:block  w-full mt-[40px]  min-h-[80vh] custom-lg:min-h-[76vh]  overflow-x-hidden">
+      <div className="hidden custom-md:block  w-full mt-[40px]  min-h-[80vh] custom-lg:min-h-[80vh]  custom-lg:mt-[20px] overflow-x-hidden">
         {/* <!-- Vertical Timeline #1 --> */}
 
         <div className="relative pl-8  py-6 group ">
