61638a1d36ecbdbd93fb9b5b0f45b41b4491800d
MoazamAli45 <21-se-48@students.uettaxila.edu.pk>
2024-01-19 02:25:20 +0500

    Update payment heading line-height for larger screens

--- app/components/Home/Payment.jsx
+++ app/components/Home/Payment.jsx
@@ -6,15 +6,10 @@ import React from "react";
 
 const Payment = () => {
   return (
-    <section className="my-[40px] overflow-x-hidden ">
+    <section className="custom-md:my-[40px] overflow-x-hidden ">
       <Wrapper>
         <div className="flex flex-col gap-[30px] items-center">
-          <Typography.H2
-            styles="text-[20px] custom-md:text-[26px] custom-xl:text-[40px] font-medium text-center custom-md:w-[70%]  mx-auto"
-            style={{
-              lineHeight: "50px",
-            }}
-          >
+          <Typography.H2 styles="text-[20px] custom-md:text-[26px] custom-xl:text-[40px] font-medium text-center custom-md:w-[70%]  mx-auto payment-heading">
             SEND PAYMENTS ANYWHERE, ANYTIME, AND HAVE IT ARRIVE IN SECONDS.
           </Typography.H2>
           <div className="relative w-[300px] max-w-[500px] h-[200px]  sm:w-[700px] sm:max-w-[800px] sm:h-[400px] custom-md:w-[1000px] xl:w-[1260px] custom-md:h-[500px] lg:h-[620px] custom-md:max-w-[1270px] ">
